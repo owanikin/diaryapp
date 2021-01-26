679d9e1e45796150af0b1a3e0445c9be074cf564
owanikin <ifeoluwa.oderinde@meltwater.org>
2021-01-26 15:22:10 +0100

    diary

---
+++ index.js
@@ -0,0 +1,33 @@
+const entryForm = document.getElementById('entryForm')
+const entriesSection = document.querySelector('#entries')
+const entryTextbox = document.querySelector('.entry-textbox')
+const entriesNav = document.querySelector('.entries-nav')
+
+
+let count = 1
+function addEntryToDom(e) {
+    e.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox.value
+    entryDiv.style.display = 'none'
+    entriesSection.appendChild(entryDiv)
+    entryTextbox.value = ''
+
+    const displayEntryButton = document.createElement('button')
+    displayEntryButton.className = 'display-entry-button'
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton)
+
+    displayEntryButton.addEventListener('click', function(){
+        const allEntries = document.querySelectorAll('.single-entry')
+        for (let i = 0; i < allEntries.length; i++) {
+            allEntries[i].style.display = 'none';
+        }
+        entryDiv.style.display = 'block';
+    })
+
+    count++
+}
+
+entryForm.addEventListener('submit', addEntryToDom)
